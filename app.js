1f9b0b6dad2a0a58a4e0938fffab59f2d64f3e95
unknown <hm3636690@gmail.com>
2025-10-18 13:19:30 +0500

    Game is completed

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
-var clutter = ""
-for(i=1; i<= 100; i++){
-    let randomNum = Math.floor(Math.random() * 10);
-    clutter += ` <div class="bubble">${randomNum}</div>`
+let gameBottom = document.querySelector(".game-bottom");
+let newScore = document.querySelector("#score");
+let score = 0;
+let hitVal = "";
+let timer = 60;
+
+gameBottom.addEventListener("click", function (e) {
+  let scoreVal = Number(e.target.textContent);
+  if (scoreVal === hitVal) {
+    increaseScore();
+    changeHit();
+    createBubbles();
+  }
+});
+
+function startTimer() {
+  let realTimer = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#myTime").textContent = timer;
+    } else {
+      gameBottom.innerHTML = `<h1>Game is Over</h1>`;
+      clearInterval(realTimer);
+    }
+  }, 1000);
 }
 
-document.querySelector(".game-bottom").innerHTML = clutter;
+function changeHit() {
+  hitVal = Math.floor(Math.random() * 10);
+  document.querySelector("#hit").innerHTML = hitVal;
+}
 
+function increaseScore() {
+  score += 10;
+  newScore.textContent = score;
+}
+
+function createBubbles() {
+  var clutter = "";
+  for (i = 1; i <= 114; i++) {
+    let randomNum = Math.floor(Math.random() * 10);
+    clutter += ` <div class="bubble">${randomNum}</div>`;
+  }
+
+  document.querySelector(".game-bottom").innerHTML = clutter;
+}
+createBubbles();
+changeHit();
+startTimer();
